06efae8871d3a9f62aa72b3900943e4391e63503
Vin Myca <vinmycasagarino@gmail.com>
2020-11-26 00:51:57 +0800

    v: added base components folder with Admin: Filter and AccResult

---
+++ client/src/components/base/AdminAccResult.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <slot name="header"></slot>
+    <v-card outlined class="acc-mgmt-cont">
+      <v-simple-table fixed-header height="300px" class="acc-mgmt-table">
+          <thead>
+            <tr>
+              <th class="text-left" style="font-size: 14px">ID</th>
+              <th class="text-left" style="font-size: 14px">Name</th>
+              <slot name="courseCol"></slot>
+              <th class="text-left" style="font-size: 14px">Email</th>
+              <th class="text-left" style="font-size: 14px">Contact</th>
+              <slot name="campusCol"></slot>
+              <slot name="edit"><th class="text-left" style="font-size: 14px">Status</th></slot>
+            </tr>
+          </thead>
+          <slot name="resultBody"></slot>
+      </v-simple-table>
+    </v-card>
+  </v-container>
+</template>
+<style scoped>
+</style>
+<script>
+export default {
+  name: "AdminMgmtResult"
+};
+</script>
