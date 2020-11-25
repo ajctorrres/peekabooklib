a2e2ec5b0c5325f2e1b77875bf09385a4c750344
Vin Myca <vinmycasagarino@gmail.com>
2020-11-26 00:54:31 +0800

    v: added sub components for Admin: filter and results

---
+++ client/src/components/ResultAdmin.vue
@@ -0,0 +1,33 @@
+<template>
+  <AdminMgmtResult>
+    <th slot="campusCol" class="text-left" style="font-size: 14px">Campus</th>
+    <th slot="edit" class="text-left" style="font-size: 14px">Option</th>
+    <tbody slot="resultBody">
+      <tr v-for="user in users" v-bind:key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.contact }}</td>
+        <td>{{ user.campus }}</td>
+        <td>
+            <v-icon>mdi-account-edit</v-icon>
+            <v-icon>mdi-close-thick</v-icon>
+        </td>
+      </tr>
+    </tbody>
+  </AdminMgmtResult>
+</template>
+<script>
+import AdminMgmtResult from "./base/AdminAccResult";
+export default {
+  name: "ResultAdmin",
+  components: {
+      AdminMgmtResult
+  },
+  props: {
+    users: {
+      type: Array,
+    },
+  }
+};
+</script>
---
+++ client/src/components/ResultFaculty.vue
@@ -0,0 +1,34 @@
+<template>
+  <AdminMgmtResult>
+    <tbody slot="resultBody">
+      <tr v-for="user in users" v-bind:key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.contact }}</td>
+        <td>
+          <span v-if="user.status == 'active'">
+            <v-icon>mdi-checkbox-blank-circle</v-icon>
+          </span>
+          <span v-else-if="user.status == 'suspended'">
+            <v-icon>mdi-minus-circle</v-icon>
+          </span> 
+        </td>
+      </tr>
+    </tbody>
+  </AdminMgmtResult>
+</template>
+<script>
+import AdminMgmtResult from "./base/AdminAccResult";
+export default {
+  name: "ResultFaculty",
+  components: {
+      AdminMgmtResult
+  },
+  props: {
+    users: {
+      type: Array,
+    },
+  }
+};
+</script>
