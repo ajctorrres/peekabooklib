f7978b6180ea5a228b1681578918fe1721da8ca7
Vin Myca <vinmycasagarino@gmail.com>
2020-11-25 16:50:57 +0800

    v: updated filter to be dynamic based on the school selected

--- client/src/views/AccMgmtStudent.vue
+++ client/src/views/AccMgmtStudent.vue
@@ -23,6 +23,7 @@
               dense
               outlined
               hide-details
+              return-object
             ></v-select>
           </v-col>
         </v-row>
@@ -38,6 +39,7 @@
               dense
               outlined
               hide-details
+              return-object
             ></v-select>
           </v-col>
           <v-col cols="4">
@@ -45,13 +47,14 @@
               v-model="selectedSchool"
               label="School"
               v-bind:items="schools"
-              item-text="school"
-              item-value="school"
+              item-text="value"
+              item-value="value"
               :menu-props="{ bottom: true, offsetY: true }"
               dense
               outlined
               hide-details
-
+              return-object
+              v-on:change="changeDept(selectedSchool)"
             ></v-select>
           </v-col>
           <v-col cols="4">
@@ -64,6 +67,7 @@
               dense
               outlined
               hide-details
+              return-object
             ></v-select>
           </v-col>
           <v-col cols="2">
@@ -76,22 +80,24 @@
               dense
               outlined
               hide-details
+              return
+              object
             ></v-select>
           </v-col>
         </v-row>
       </v-card>
-      
+
       <v-card outlined class="acc-mgmt-cont">
         <v-simple-table fixed-header height="300px" class="acc-mgmt-table">
           <template v-slot:default>
             <thead>
               <tr>
-                <th class="text-left" style="font-size:14px;">ID</th>
-                <th class="text-left" style="font-size:14px;">Name</th>
-                <th class="text-left" style="font-size:14px;">Course</th>
-                <th class="text-left" style="font-size:14px;">Email</th>
-                <th class="text-left" style="font-size:14px;">Contact</th>
-                <th class="text-left" style="font-size:14px;">Status</th>
+                <th class="text-left" style="font-size: 14px">ID</th>
+                <th class="text-left" style="font-size: 14px">Name</th>
+                <th class="text-left" style="font-size: 14px">Course</th>
+                <th class="text-left" style="font-size: 14px">Email</th>
+                <th class="text-left" style="font-size: 14px">Contact</th>
+                <th class="text-left" style="font-size: 14px">Status</th>
               </tr>
             </thead>
             <tbody>
@@ -118,40 +124,236 @@ export default {
     return {
       input: "",
       newInput: "",
-      school: '',
-      selectedSchool: '',
       showNum: [{ num: "10" }, { num: "30" }, { num: "50" }, { num: "100" }],
       year_levels: [
-        { lvl: "All" },
+        { lvl: "All"},
         { lvl: "First Year" },
         { lvl: "Second Year" },
         { lvl: "Third Year" },
         { lvl: "Fourth Year" },
-        { lvl: "Fifth Year" },
+        { lvl: "Fifth Year" }
       ],
+      yearData: {
+        BED: [
+          { lvl: "All"}
+        ],
+        College: [
+          { lvl: "All"},
+          { lvl: "First Year" },
+          { lvl: "Second Year" },
+          { lvl: "Third Year" },
+          { lvl: "Fourth Year" },
+          { lvl: "Fifth Year" }
+        ]
+      },
+      selectedSchool: "",
+      school: "",
       schools: [
-        { school: "All" },
-        { school: "Basic Education" },
-        { school: "School of Architecture, Fine Arts and Design" },
-        { school: "School of Arts and Sciences" },
-        { school: "School of Business and Economics" },
-        { school: "School of Education" },
-        { school: "School of Engineering" },
-        { school: "School of Healthcare Professions" },
-        { school: "School of Law and Governance" },
+        { value: "All" },
+        { value: "Basic Education" },
+        { value: "School of Architecture, Fine Arts and Design" },
+        { value: "School of Arts and Sciences" },
+        { value: "School of Business and Economics" },
+        { value: "School of Education" },
+        { value: "School of Engineering" },
+        { value: "School of Healthcare Professions" },
+        { value: "School of Law and Governance" },
       ],
-      departments: [{ dept: "All" }],
-      status: [{ stat: "All" }, { stat: "Active" }, { stat: "Disabled" }],
+      departments: [
+          { dept: "All" },
+          { dept: "Department of Architecture" },
+          { dept: "Department of Fine Arts" },
+
+          { dept: "Department of Anthropology, Sociology and History" },
+          { dept: "Department of Biology" },
+          { dept: "Department of Chemistry" },
+          { dept: "Department of Communications, Linguistics and Literature" },
+          { dept: "Department of Computer, Information Sciences and Mathematics" },
+          { dept: "Department of General Education and Mission" },
+          { dept: "Department of Philosophy" },
+          { dept: "Department of Physics" },
+          { dept: "Department of Psychology" },
+          { dept: "Mathematics Section" },
+
+          { dept: "Department of Accountancy" },
+          { dept: "Department of Business Administration" },
+          { dept: "Department of Economics" },
+          { dept: "Department of Hospitality Management" },
+
+          { dept: "Department of Science and Mathematics Education" },
+          { dept: "Department of Teacher Education" },
+
+          { dept: "Department of Chemical Engineering" },
+          { dept: "Department of Civil Engineering" },
+          { dept: "Department of Computer Engineering" },
+          { dept: "Department of Electrical and Electronics Engineering" },
+          { dept: "Department of Industrial Engineering" },
+
+          { dept: "Department of Nursing" },
+          { dept: "Department of Nutrition and Dietetics" },
+          { dept: "Department of Pharmacy" },
+
+          { dept: "Department of Political Science" },
+          { dept: "Law" }
+      ],
+      departmentData: {
+        All: [
+          { dept: "All" },
+          { dept: "Department of Architecture" },
+          { dept: "Department of Fine Arts" },
+
+          { dept: "Department of Anthropology, Sociology and History" },
+          { dept: "Department of Biology" },
+          { dept: "Department of Chemistry" },
+          { dept: "Department of Communications, Linguistics and Literature" },
+          { dept: "Department of Computer, Information Sciences and Mathematics" },
+          { dept: "Department of General Education and Mission" },
+          { dept: "Department of Philosophy" },
+          { dept: "Department of Physics" },
+          { dept: "Department of Psychology" },
+          { dept: "Mathematics Section" },
+
+          { dept: "Department of Accountancy" },
+          { dept: "Department of Business Administration" },
+          { dept: "Department of Economics" },
+          { dept: "Department of Hospitality Management" },
+
+          { dept: "Department of Science and Mathematics Education" },
+          { dept: "Department of Teacher Education" },
+
+          { dept: "Department of Chemical Engineering" },
+          { dept: "Department of Civil Engineering" },
+          { dept: "Department of Computer Engineering" },
+          { dept: "Department of Electrical and Electronics Engineering" },
+          { dept: "Department of Industrial Engineering" },
+
+          { dept: "Department of Nursing" },
+          { dept: "Department of Nutrition and Dietetics" },
+          { dept: "Department of Pharmacy" },
+
+          { dept: "Department of Political Science" },
+          { dept: "Law" }
+        ],
+        BED: [
+          { dept: "All" }
+        ],
+        SAFAD: [
+          { dept: "All" },
+          { dept: "Department of Architecture" },
+          { dept: "Department of Fine Arts" }
+        ],
+        SAS: [
+          { dept: "All" },
+          { dept: "Department of Anthropology, Sociology and History" },
+          { dept: "Department of Biology" },
+          { dept: "Department of Chemistry" },
+          { dept: "Department of Communications, Linguistics and Literature" },
+          { dept: "Department of Computer, Information Sciences and Mathematics" },
+          { dept: "Department of General Education and Mission" },
+          { dept: "Department of Philosophy" },
+          { dept: "Department of Physics" },
+          { dept: "Department of Psychology" },
+          { dept: "Mathematics Section" }
+        ],
+        SBE: [
+          { dept: "All" },
+          { dept: "Department of Accountancy" },
+          { dept: "Department of Business Administration" },
+          { dept: "Department of Economics" },
+          { dept: "Department of Hospitality Management" }
+        ],
+        SED: [
+          { dept: "All" },
+          { dept: "Department of Science and Mathematics Education" },
+          { dept: "Department of Teacher Education" }
+        ],
+        SOE: [
+          { dept: "All" },
+          { dept: "Department of Chemical Engineering" },
+          { dept: "Department of Civil Engineering" },
+          { dept: "Department of Computer Engineering" },
+          { dept: "Department of Electrical and Electronics Engineering" },
+          { dept: "Department of Industrial Engineering" }
+        ],
+        SHCP: [
+          { dept: "All" },
+          { dept: "Department of Nursing" },
+          { dept: "Department of Nutrition and Dietetics" },
+          { dept: "Department of Pharmacy" }
+        ],
+        SLG: [
+          { dept: "All" },
+          { dept: "Department of Political Science" },
+          { dept: "Law" }
+        ]
+      },
+
+      status: [{ stat: "All" }, { stat: "Active" }, { stat: "Suspended" }, { stat: "Removed"}],
       students: [
-        { id: '00000', name: 'Vin', course: 'BSCS', email: 'vin@gmail.com', contact: '712635', status: true}
-      ]
+        {
+          id: "00000",
+          name: "Vin",
+          course: "BSCS",
+          email: "vin@gmail.com",
+          contact: "712635",
+          status: true,
+        },
+      ],
     };
   },
   methods: {
     inputSearch: function (newInput) {
       this.input = newInput;
+    },
+    changeDept: function (selectedSchool) {
+      switch(selectedSchool.value) {
+         case 'All':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.All
+          break;
+
+        case 'Basic Education':
+          this.year_levels = this.yearData.BED
+          this.departments = this.departmentData.BED
+          break;
+
+        case 'School of Architecture, Fine Arts and Design':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SAFAD
+          break;
+
+        case 'School of Arts and Sciences':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SAS
+          break;
+
+        case 'School of Business and Economics':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SBE
+          break;
+        
+        case 'School of Education':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SED
+          break;
+
+        case 'School of Engineering':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SOE
+          break;
+        
+        case 'School of Healthcare Professions':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SHCP
+          break;
+        
+        case 'School of Law and Governance':
+          this.year_levels = this.yearData.College
+          this.departments = this.departmentData.SLG
+          break;
+      }
     }
-  },
+  }
 };
 </script>
 <style scoped>
