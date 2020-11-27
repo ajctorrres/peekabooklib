5961a6dcb7ac79d2b8c986a1c2c9e6a7bb83f358
Vin Myca <vinmycasagarino@gmail.com>
2020-11-27 12:47:41 +0800

    v: removed duplicate

--- client/src/main.js
+++ client/src/main.js
@@ -6,23 +6,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import { BootstrapVue, BIcon, BIconBell, BIconBookmarkHeartFill, BIconSearch, BIconCaretDownFill} from 'bootstrap-vue'
+import { BootstrapVue, BIcon, BIconBell, BIconBookmarkHeartFill, BIconSearch, BIconCaretDownFill, IconsPlugin} from 'bootstrap-vue'
 
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
-<<<<<<< HEAD
 Vue.component('BIcon', BIcon)
 Vue.component('BIconBell', BIconBell)
 Vue.component('BIconBookmarkHeartFill', BIconBookmarkHeartFill)
 Vue.component('BIconSearch', BIconSearch)
 Vue.component('BIconCaretDownFill', BIconCaretDownFill)
-=======
 Vue.use(IconsPlugin)
 Vue.use(vuetify)
 
@@ -33,7 +29,6 @@ import '@fortawesome/fontawesome-free/css/all.css'
 Vue.use(vuetify, {
   iconfont: 'fa'
 })
->>>>>>> 6195854e76ee7106621d690f1ae1f1b6b786785b
 
 new Vue({
   router,
